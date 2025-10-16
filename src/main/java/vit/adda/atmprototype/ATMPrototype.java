4a45430c89985aefd93f0e899f69d58a3413a595
harsh <harsh@HarshGangwar>
2025-10-16 13:25:44 +0530

    Added ATM.java and Account.java classes

---
+++ src/main/java/vit/adda/atmprototype/ATMPrototype.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vit.adda.atmprototype;
+
+/**
+ *
+ * @author harsh
+ */
+public class ATMPrototype {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
